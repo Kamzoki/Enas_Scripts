77073d57bb1910c783a6b5f89d75f0fc12c802ea
MAX ELDESOKY <MAX ELDESOKY>
2015-12-21 01:03:49 +0200

    i made a standerd health script

---
+++ Health.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	//Private health variable
+	private int health=10;
+	
+	void Update () {
+		
+	}
+	//Properties to access and validate variable
+	public int MyHealth {
+		
+		//If reading variable then return health
+		get{return health;}
+		//Chang health value, now validate
+		set{//Update health
+		health = value;}
+	}
+
+}
+
