7edce2ae5de89024a00b31e966004a1aa7715eb6
Kamzoki <kaittah@ymail.com>
2016-01-23 19:33:04 +0200

    Back to work

---
+++ LoadingBar.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class LoadingBar : MonoBehaviour {
+
+	public float loadingTime;
+	private double fill;
+	public string SceneToLoad;
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Image> ().fillAmount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.GetComponent<Image>().fillAmount <= 1.0)
+			gameObject.GetComponent<Image>().fillAmount += 0.1f;
+		if (gameObject.GetComponent<Image> ().fillAmount == 1.0)
+			LoadScene ();
+	}
+
+	
+	public void LoadScene(){
+		Application.LoadLevel (SceneToLoad);
+	}
+}
---
+++ SplashScreen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+	public GameObject cnv;
+
+	public void onStartEnter(){
+		GameObject.Destroy (gameObject.GetComponent<Canvas>());
+		cnv.SetActive (true);
+	}
+}
